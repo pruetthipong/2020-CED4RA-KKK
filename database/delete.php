a5eeb0584facebc7c9170cffb8118030f3b861ff
pukky060342 <s6002041620071@email.kmutnb.ac.th>
2020-08-13 13:34:32 +0700

    Update delete.php

--- database/delete.php
+++ database/delete.php
@@ -1,10 +1,25 @@
+<?php 
+    session_start();
+
+    if (!isset($_SESSION['username'])) {
+        $_SESSION['msg'] = "You must log in first";
+        header('location: login.php');
+    }
+
+    if (isset($_GET['logout'])) {
+        session_destroy();
+        unset($_SESSION['username']);
+        header('location: login.php');
+    }
+?>
 <?php   
   //including the database connection file   
-  include("config.php");   
+  
+  include("server.php");   
   //getting id of the data from url   
   $id = $_GET['id'];   
   //deleting the row from table   
-  $result = mysqli_query($mysqli, "DELETE FROM users WHERE id=$id");   
+  $result = mysqli_query($mysqli, "DELETE FROM user WHERE id=$id");   
   //redirecting to the display page (index.php in our case)   
   header("Location:index.php");   
   ?>   
