4c5e540773a7f455b9990c099e79d81f59bd2868
Pruetthipong <fernfernzaa16@hotmail.com>
2020-08-13 16:23:19 +0700

    Up

--- database/server.php
+++ database/server.php
@@ -1,8 +1,22 @@
+ <!-- <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "newnormal";
 
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+?>  -->
 
 <?php   
   $databaseHost = 'localhost';   
-  $databaseName = 'patcha_db';   
+  $databaseName = 'newnormal';   
   $databaseUsername = 'root';   
   $databasePassword = '';   
   
